b5508d871621a9fa3e2b5a5893d9dfcdf5b48859
hounghoung04a <hounghoung04a@gmail.com>
2026-07-20 04:36:09 +0700

    Fix order list for staff at previous days 2

--- src/main/java/com/koicoffee/backend/repository/OrderRepository.java
+++ src/main/java/com/koicoffee/backend/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.koicoffee.backend.repository;
 
-import com.koicoffee.backend.model.Order;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -11,8 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import com.koicoffee.backend.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
