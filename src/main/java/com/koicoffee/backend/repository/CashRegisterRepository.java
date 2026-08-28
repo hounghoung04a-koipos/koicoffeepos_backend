21df09b7d41c7ea140b146229a7671a2629b8e7f
KhuongNguyen8190 <143570794+KhuongNguyen8190@users.noreply.github.com>
2026-08-28 09:05:09 +0700

    Delete src/main/java/com/koicoffee directory

--- src/main/java/com/koicoffee/backend/repository/CashRegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.koicoffee.backend.repository;
-
-import com.koicoffee.backend.model.CashRegister;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CashRegisterRepository extends JpaRepository<CashRegister, Long> {
-}
