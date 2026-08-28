d7c99208fba7296449f834609384c26317c32280
KhuongNguyen8190 <143570794+KhuongNguyen8190@users.noreply.github.com>
2026-08-28 09:05:35 +0700

    Update KoiCoffeeBackendApplicationTests.java

--- src/test/java/com/koicoffee/backend/KoiCoffeeBackendApplicationTests.java
+++ src/test/java/com/koicoffee/backend/KoiCoffeeBackendApplicationTests.java
@@ -1,13 +1,1 @@
-package com.koicoffee.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KoiCoffeeBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
